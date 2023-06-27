9632311c70cbf873190386caa9eb4181dd7d2bf9
Bharath2938 <bharath9597120836@gmail.com>
2023-06-27 07:38:12 -0700

    Title logo changed

--- src/Routes/Project.js
+++ src/Routes/Project.js
@@ -1,33 +1,34 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import Navbar from '../Header/Navbar';
-import HeroImg2 from '../Components/HeroImages/HeroImg2';
-import ProjectCarousel from '../Components/ProjectCarousel';
-import Portfolio from '../Components/Portfolio';
-import Loader from '../Components/Loader';
+import React from "react";
+import { useState, useEffect } from "react";
+import Navbar from "../Header/Navbar";
+import HeroImg2 from "../Components/HeroImages/HeroImg2";
+import ProjectCarousel from "../Components/ProjectCarousel";
+import Portfolio from "../Components/Portfolio";
+import Loader from "../Components/Loader";
 
 const Project = () => {
-  const [loading , setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(()=>{
-    setTimeout(() =>{
+  useEffect(() => {
+    setTimeout(() => {
       setLoading(false);
-    },1200);
-  },[]);
+    }, 1200);
+  }, []);
 
   return (
     <div>
-       {loading ? (<Loader />) : (<>
-        <Navbar />
-      <HeroImg2 />
-      <ProjectCarousel />
-      <Portfolio />
-       </>
-      
-       )
-}
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Navbar />
+          <HeroImg2 />
+          <ProjectCarousel />
+          <Portfolio />
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
